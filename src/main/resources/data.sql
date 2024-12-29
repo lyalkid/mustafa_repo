29fe1cd2e705c85ad21612a1c9bdfc88ea537781
Гайфуллин Азамат Ильдарович AIGayfullin <agaifullin157@gmail.com>
2024-12-29 15:55:35 +0300

    added new tables

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,24 +1,56 @@
-create table users (
-    id serial primary key,
+create table users
+(
+    id        serial primary key,
     user_name varchar(100) unique not null,
-    email varchar(255) unique not null,
-    password varchar(255) not null
+    email     varchar(255) unique not null,
+    password  varchar(255)        not null
 );
 
-create table announcement(
-    id serial primary key,
-    name varchar(100) unique not null,
-    description text
+create table announcement
+(
+    id          serial primary key,
+    name        varchar(100) unique not null,
+    description text,
+    user_id     int,
+    foreign key (user_id) references users (id) on delete cascade
 
 );
 
-create table user_and_announcement(
+
+
+create table personal_data
+(
+    id           serial primary key,
+    user_id      int,
+    first_name   varchar(100) NOT NULL,
+    second_name  varchar(100) not null,
+    birth_date   date,
+    phone_number varchar(100),
+    foreign key (user_id) references users (id) on delete cascade
+);
+
+
+create table favorites
+(
+    id              serial primary key,
+    user_id         int,
+    announcement_id int,
+    unique (user_id, announcement_id),
+    foreign key (user_id) references users (id) on delete cascade,
+    foreign key (announcement_id) references announcement (id) on delete cascade
+
+);
+
+create table reviews(
     id serial primary key,
+    rate int,
+    comment text,
     user_id int,
     announcement_id int,
-    foreign key (user_id) references users(id) on delete cascade ,
-    foreign key (announcement_id) references announcement(id) on delete cascade
+    foreign key (user_id) references users (id) on delete cascade,
+    foreign key (announcement_id) references announcement (id) on delete cascade
 );
 
 
-select * from users;
+select *
+from users;
